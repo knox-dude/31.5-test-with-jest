ba6c4ecfda6cae1025cbe187aa35675a68a1de2a
Andrew Knox <andrew.knox.r@gmail.com>
2024-02-16 12:53:28 -0800

    added markovMachine

--- markov.js
+++ markov.js
@@ -1,13 +1,11 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
+    this.words = words.filter((c) => c !== "");
     this.makeChains();
   }
 
@@ -17,13 +15,58 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
-  }
+    let curr_word;
+    let next_word;
+    this.chains = {};
 
+    for (let i = 0; i < this.words.length; i++) {
+
+      curr_word = this.words[i];
+
+      if (i + 1 == this.words.length) {
+        next_word = null
+      } else {
+        next_word = this.words[i+1]
+      }
+
+      if (!this.chains[curr_word]) {
+        let new_arr = [];
+        this.chains[curr_word] = new_arr;
+      } 
+      this.chains[curr_word].push(next_word)
+    }
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let curr_num = 0;
+    let generated_text = "";
+    let prev = null;
+    let curr = null;
+    let curr_choices;
+
+    while (curr_num < numWords) {
+      if (prev == null) {
+        const keys = Object.keys(this.chains);
+        curr_choices = this.chains[keys[Math.floor(Math.random()*keys.length)]];
+      }
+      else {
+        curr_choices = this.chains[prev];
+      }
+
+      curr = curr_choices[Math.floor(Math.random()*curr_choices.length)];
+      prev=curr;
+
+      if (!curr) {
+        break;
+      }
+
+      generated_text += curr + " ";
+
+    }
+
+  return generated_text;
+
   }
 }
